90e0e03bd619642566a55215d70446874847d718
gorille02 <sebastien.gadot@gmail.com>
2015-03-24 21:23:05 +0100

    scripts installation

---
+++ install.bat
@@ -0,0 +1,12 @@
+if exist %userprofile%\desktop\gestion-paroisse goto update
+cd  %userprofile%\desktop
+call git clone https://github.com/gorille/gestion-paroisse.git
+cd %userprofile%\desktop\gestion-paroisse
+
+call rake db:create
+
+
+:update
+cd %userprofile%\desktop\gestion-paroisse
+call rake db:migrate
+call bundle install
---
+++ start.bat
@@ -0,0 +1,3 @@
+start bundle exec thin start
+call ruby -e sleep 10
+start http://localhost:3000
