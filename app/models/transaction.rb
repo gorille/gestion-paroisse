52eb59bcafa9ad515be8cfd02a50fc8081390545
gorille02 <sebastien.gadot@gmail.com>
2015-03-31 21:23:31 +0200

    tri des morts et donnateurs

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ActiveRecord::Base
-  belongs_to :mort
-  belongs_to :donnateur
+  belongs_to :mort, ->{ order(nom: :asc)}
+  belongs_to :donnateur, ->{ order(nom: :asc)}
   belongs_to :clocher
   validates :mort, presence: true
 end
